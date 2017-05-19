68aa7f9fe0147ebca7c5997a92c9cc716d73a159
Nick M <nicholas.maschinot@gmail.com>
2017-05-19 03:46:38 +0000

    stop point for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -58,7 +58,7 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end 
   
-  test "password should be present" do
+  test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
