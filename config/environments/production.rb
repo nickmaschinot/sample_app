f236d09858029a3d97d1ddc877a47e624cbc3729
Nick M <nicholas.maschinot@gmail.com>
2017-05-20 06:17:50 +0000

    try five

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
     :address               => 'smtp.sendgrid.net',
     :port                  => '587',
     :authentication        => :plain,
-    :user_name             => ENV['SENDGIRD_USERNAME'],
-    :password              => ENV['SENDGIRD_PASSWORD'],
+    :user_name             => ENV['SENDGRID_USERNAME'],
+    :password              => ENV['SENDGRID_PASSWORD'],
     :domain                => 'heroku.com',
     :enable_starttls_auto  => true
     
