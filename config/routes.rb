d43733ba13fa59c545df45b70e44264644a39d27
Nick M <nicholas.maschinot@gmail.com>
2017-05-20 21:10:34 +0000

    Try four

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'password_resets/now'
+ # get 'password_resets/new'
 
-  get 'password_resets/edit'
+ # get 'password_resets/edit'
 
-  get 'sessions/new'
+ # get 'sessions/new'
 
-  get 'users/new'
+ # get 'users/new'
 
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
