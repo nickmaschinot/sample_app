6d6fcc56ac22c6475c42cfdcbc7d790e718b514d
Nick M <nicholas.maschinot@gmail.com>
2017-05-20 04:24:10 +0000

    Fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = still-thicket-35313.herokuapp.com
+  host = "still-thicket-35313.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address               => 'smtp.sendgrid.net',
